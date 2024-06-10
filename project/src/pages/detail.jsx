3b6bd28a7058f4f8164881c95fc9ea872641982a
Mohamed-Mesrar2 <mohamedmesrar2002@gmaile.com>
2024-06-10 20:11:46 +0100

    test

--- project/src/pages/detail.jsx
+++ project/src/pages/detail.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
 function Detail(){
-    return (
-        <div>
-            <img src='s.jpeg' className="log"  height="100%" width="50%"/>
-         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur nostrum temporibus quaerat nesciunt ad. Sed, <br />
+    return (<>
+        <div width="50px" style={{display:"flex"}} >
+            <img src='s.jpeg' className="log" width={"700px"} />
+         <p style={{padding:33}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur nostrum temporibus quaerat nesciunt ad. Sed, <br />
          nostrum eius dolores ratione ipsa non autem veritatis, optio dolorum esse voluptatem <br />
-         itaque repudiandae in.</p>
-         <Link to="/home">ajeter en stocke</Link>
+         itaque repudiandae in.
+         <br /><br />
+         <Link style={{padding:20,backgroundColor:"black",borderRadius:20}} to="/home">ajeter en stocke</Link>
+         </p>
+
         </div>
+         </>
     )
 };
 export default Detail;
